38a7e009286de8c1e1fdb294b725b92a3680c079
Martin Toennishoff <chocokiko4@gmx.de>
2018-04-26 09:24:39 +0000

    add sonarqube config

---
+++ sonar.sh
@@ -0,0 +1,6 @@
+~/Desktop/trial_symfony4/sonar/sonar-scanner-3.1.0.1141-linux/bin/sonar-scanner \
+  -Dsonar.projectKey=trial_symfony4 \
+  -Dsonar.organization=pennyarcade-github \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=https://sonarcloud.io \
+  -Dsonar.login=c69eedafa987696aa09015fe8c1ff03d0c8c895c
